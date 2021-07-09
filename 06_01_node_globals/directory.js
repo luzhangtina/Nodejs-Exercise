6a60b9485948eea4bce4668296be0b60c5c1491d
tinaz <zhanglu.xidian@gmail.com>
2021-07-09 21:42:32 +1000

    Use global object: __dirname, __filename. Require path module.

---
+++ 06_01_node_globals/directory.js
@@ -0,0 +1,5 @@
+var path = require('path')
+
+console.log(`The directory name is ${__dirname}`)
+console.log(`The absolute file name is ${__filename}`)
+console.log(`The file name is ${path.basename(__filename)}`)
