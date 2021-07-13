97837e045a143e1bb0ac8337e8e1355a43e93f15
tinaz <zhanglu.xidian@gmail.com>
2021-07-13 23:28:23 +1000

    consume event

--- 06_04_create_module/askQuestion.js
+++ 06_04_create_module/askQuestion.js
@@ -6,4 +6,19 @@ const questions = [
     'What are you going to do with node js? '
 ]
 
-collectAnswers(questions)
+const answerEvent = collectAnswers(questions)
+
+answerEvent.on('answer', (answer) => {
+    console.log(`Question answered: ${answer}`)
+})
+
+// First subscription on complete event
+answerEvent.on('complete', (answers) => {
+    console.log(`Here are all answers: ${answers}`)
+})
+
+// Second subscription on complete event
+answerEvent.on('complete', (answers) => {
+    console.log(`Thanks for your answers!`)
+    process.exit()
+})
