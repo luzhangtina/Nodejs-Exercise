f24daf23cc102763c346a3e2af2a792c9cdebeca
tinaz <zhanglu.xidian@gmail.com>
2021-07-13 23:02:27 +1000

    Use created module

---
+++ 06_04_create_module/askQuestion.js
@@ -0,0 +1,9 @@
+const collectAnswers = require('./lib/collectAnswers')
+
+const questions = [
+    'What is your name? ',
+    'Where do you live? ',
+    'What are you going to do with node js? '
+]
+
+collectAnswers(questions)
