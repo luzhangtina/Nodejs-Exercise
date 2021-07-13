f543e5e3a544bfd3802d2f4755905cf41ff3fc05
tinaz <zhanglu.xidian@gmail.com>
2021-07-13 22:37:38 +1000

    Use readline module to simplify Q&A program

--- 06_02_modules/readline.js
+++ 06_02_modules/readline.js
@@ -5,6 +5,29 @@ const rl = readline.createInterface({
     output: process.stdout
 })
 
-rl.question('How do you like Node? ', answer => {
-    console.log(`Your answer: ${answer}`)
+const questions = [
+    'What is your name? ',
+    'Where do you live? ',
+    'What are you going to do with node js? '
+]
+
+const collectAnswers = (questions, done) => {
+    const answers = []
+    const [firstQuestion] = questions;
+
+    const questionAnswered = (answer) => {
+        answers.push(answer)
+        if (answers.length < questions.length) {
+            rl.question(questions[answers.length], questionAnswered)
+        } else {
+            done(answers)
+        }
+    }
+    rl.question(firstQuestion, questionAnswered)
+}
+
+collectAnswers(questions, answers => {
+    console.log('Thank you for your answers. ');
+    console.log(answers)
+    process.exit()
 })
