e5c359e2d30790c936508ee66d5088cbc5bf5b1c
tinaz <zhanglu.xidian@gmail.com>
2021-07-28 17:56:28 +1000

    add first routing module

--- 08_01_build_website_with_express/server.js
+++ 08_01_build_website_with_express/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 
+const routes = require('./routes');
+
 const app = express();
 
 const port = 3000;
@@ -9,14 +11,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'));
 
 app.use(express.static(path.join(__dirname, './static')));
-
-app.get('/', (request, response) => {
-  response.render('pages/index', { pageTitle: 'Welcome' });
-});
-
-app.get('/speakers', (request, response) => {
-  response.sendFile(path.join(__dirname, './static/speaker.html'));
-});
+app.use('/', routes());
 
 const server = app.listen(port, () => {
   console.log(`Express server is listening on port ${server.address().port}`);
