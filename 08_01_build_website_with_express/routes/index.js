7d4b913ea54758ffde91192385c6209d1b7ad6a9
tinaz <zhanglu.xidian@gmail.com>
2021-07-31 20:08:55 +1000

    add session management

--- 08_01_build_website_with_express/routes/index.js
+++ 08_01_build_website_with_express/routes/index.js
@@ -6,11 +6,12 @@ const feedbackRoute = require('./feedback');
 const router = express.Router();
 
 module.exports = (params) => {
-  router.use('/speakers', speakersRoute(params));
-  router.use('/feedback', feedbackRoute(params));
   router.get('/', (request, response) => {
     response.render('pages/index', { pageTitle: 'Welcome' });
   });
+
+  router.use('/speakers', speakersRoute(params));
+  router.use('/feedback', feedbackRoute(params));
 
   return router;
 };
--- 08_01_build_website_with_express/server.js
+++ 08_01_build_website_with_express/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const cookieSession = require('cookie-session');
 
 const FeedbackService = require('./services/FeedbackService');
 const SpeakersService = require('./services/SpeakerService');
@@ -12,6 +13,16 @@ const routes = require('./routes/index');
 const app = express();
 
 const port = 3000;
+
+// To tell express to trust cookie passed through reverse proxy
+app.set('trust proxy', 1);
+
+app.use(
+  cookieSession({
+    name: 'session',
+    keys: ['GHASDFJADSeiffg', 'hhktjdaskdfjgadfjkasdjf'],
+  })
+);
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'));
